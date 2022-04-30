59722f7e764d75c08526dcb97e1a0a5f9bbe54df
Molnár Kristóf <molnarok2313@gmail.com>
2022-04-30 23:45:04 +0200

    movie card done

--- frontend/src/app/movie-card/movie-card.component.ts
+++ frontend/src/app/movie-card/movie-card.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import {movie, partial_movie} from "../../misc/models";
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss']
 })
 export class MovieCardComponent implements OnInit {
-
+  @Input() movie?: partial_movie;
   constructor() { }
-
   ngOnInit(): void {
   }
 
+  openMovieDialog(): void{
+
+  }
 }
