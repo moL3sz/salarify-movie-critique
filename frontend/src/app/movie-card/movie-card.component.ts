13bb31d3a1726af5ec9280758df1c1bfe65baf9e
Molnár Kristóf <molnarok2313@gmail.com>
2022-05-01 10:57:21 +0200

    opens dialog

--- frontend/src/app/movie-card/movie-card.component.ts
+++ frontend/src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {movie, partial_movie} from "../../misc/models";
+import { partial_movie} from "../../misc/models";
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import {MovieDescriptionCardComponent} from "../movie-description-card/movie-description-card.component";
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
@@ -7,11 +9,17 @@ import {movie, partial_movie} from "../../misc/models";
 })
 export class MovieCardComponent implements OnInit {
   @Input() movie?: partial_movie;
-  constructor() { }
+  constructor(public movieDialog: MatDialog) { }
   ngOnInit(): void {
+    return
   }
 
   openMovieDialog(): void{
+    const movieDialogRef = this.movieDialog.open(MovieDescriptionCardComponent,{
+      data:{
+        id:this.movie?.id
+      }
+    })
 
   }
 }
